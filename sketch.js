c7eeb7882ab9f4bb8afc79539f6ee24ceef6783e
Adrybiel <00001132756534sp@al.educacao.sp.gov.br>
2024-09-20 10:33:17 -0300

    sitio.html

--- sketch.js
+++ sketch.js
@@ -1,18 +1,36 @@
-let cor
+let texto = "T    T"
+let texto2 = "_____"
+let texto3 = "Não aperte em nada"
+let texto4 = "'-'"
+let texto5 = "não sei oque fazer"
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(400, 400);  
 }
 
 function draw() {
-  background(145, 0, 255);
-  cor = color(random(0, 255), random(0, 255), random(0, 255));
-}
-
-// função de desenhar
-
-function draw() {
-  if(mouseIsPressed)
-    circle(mouseX, mouseY, 10, 10)
-    fill(0)
+  fill("white");
+  background("black");
+  circle(200, 200, 400);
+  fill("black");
+  textSize(64);
+  textAlign(CENTER, CENTER);
+  text(texto, 200, 200);
+  text(texto2, 200, 220);
+  textSize(32);
+  text(texto3, 200, 300);
+  text(texto5, 200, 100)
+  
+//AI
+  
+//console.log(mouseIsPressed);
+  fill("white");
+  rect(mouseX, mouseY, 5, 5);
+  if(mouseIsPressed) {
+    fill("white");
+    circle(200, 200, 400);
+    fill("black");
+    textSize(64);
+    text(texto4, 200, 200);
+  }
 }
